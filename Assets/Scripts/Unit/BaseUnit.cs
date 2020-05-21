29063ec8efecfb35d6b301d216655c1fc37e1e73
wilybear <samerj9712@gmail.com>
2020-05-21 21:03:08 +0900

    Inital_and Units
    first commit

---
+++ Assets/Scripts/Unit/BaseUnit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+    public class BaseUnit : MonoBehaviour
+    {
+        //x,y,z
+        public int width = 1;
+        public int length = 1;
+        public int height = 1;
+
+        private int sortedOrder;
+        
+        public int order
+        {
+            get { return sortedOrder; }
+            set
+            {
+                sortedOrder = value;
+                foreach (Transform child in transform)
+                {
+                    child.GetComponent<Renderer>().sortingOrder = value;
+                }
+            }
+        }
+
+        public Tile origin { get; protected set; }
+        
+        //unit이 가지고 있는 타일들
+        protected List<Tile> tiles = new List<Tile>();
+        
+        public List<Tile> GetTiles()
+        {
+            return tiles;
+        }
+            
+      }
+ }
---
+++ Assets/Scripts/Unit/Furniture.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Model
+{
+    //동서남북
+    public enum Direction : int
+    {
+        South,
+        West,
+        North,
+        East,
+    }
+
+    //Base Unit 상속
+    public class Furniture : BaseUnit
+    {
+
+        //각 방향에 대한 게임 오브젝트들
+        public GameObject[] ListDirectionItem = new GameObject[4];
+
+        //south가 기본값
+        private Direction direction = Direction.South;
+
+        //이전 값? 잘 모르겠당
+        public HistoricalData previous { get; private set; }
+
+        private List<GameObject> blocks;
+
+        private const string Unit_LAYER = "Unit";
+        private const string PREVIEW_LAYER = "Preview";
+
+        //회전
+        public void Rotate()
+        {
+            direction = (Direction)(((int)direction + 1) % 4);
+
+            foreach (var dir in ListDirectionItem)
+                dir.SetActive(false);
+
+            //gameobject들중 해당 direction만 남기고 나머지 setActive false
+            ListDirectionItem[(int)direction].SetActive(true);
+            //가로 세로 조정
+            var temp = width;
+            width = length;
+            length = temp;
+        }
+
+
+        //뭐하는 함수지
+        public void Rotate(Direction dir)
+        {
+            //dir파라미터 - 현재 방향 의 절대값의 2로 나눈 몫이 1이면
+            if (Mathf.Abs(dir - direction) % 2 == 1)
+            {
+                var temp = width;
+                width = length;
+                length = temp;
+            }
+            direction = dir;
+
+            foreach (var diritem in ListDirectionItem)
+                diritem.SetActive(false);
+
+            ListDirectionItem[(int)dir].SetActive(true);
+        }
+
+        //게임오브젝트의 위치를 타일의 위치로 옮긴다.
+        public void Move(Tile tile)
+        {
+            gameObject.transform.position = tile.transform.position;
+            origin = tile;
+        }
+
+        //각 방향별 게임오브젝트마다 색깔을 바꾸어줌
+        public void SetColor(Color color)
+        {
+            foreach (var dir in ListDirectionItem)
+                dir.GetComponent<SpriteRenderer>().color = color;
+        }
+
+
+        public void Place(List<Tile> tiles)
+        {
+            base.tiles = tiles;
+            //물체가 있는 타일을 block시킨다
+            base.tiles.ForEach(tile => tile.isBlock = true);
+
+            //랜더링 시키기?
+            //layer 호가정
+            foreach (var dir in ListDirectionItem)
+                dir.GetComponent<Renderer>().sortingLayerName = Unit_LAYER;
+
+            //취소했을때를 위하여
+            previous = new HistoricalData(origin, direction);
+            //create box collider in 3D.
+            Block(tiles);
+        }
+
+        public void Unplaced()
+        {
+            tiles.ForEach(tile => tile.isBlock = false);
+            tiles = new List<Tile>();
+
+            //PREVIEW로 옮김
+            foreach (var dir in ListDirectionItem)
+                dir.GetComponent<Renderer>().sortingLayerName = PREVIEW_LAYER;
+
+            UnBlock();
+        }
+
+        //이 함수는 무엇이냐? navigation을 위하여 물체에 큐브를 두어 캐릭터가 못지나가게 막아주는것
+        private void Block(List<Tile> tiles)
+        {
+            blocks = new List<GameObject>();
+            foreach (var tile in tiles)
+            {
+                var block = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                block.transform.SetParent(GameObject.Find("AIManager").transform);
+                block.transform.localEulerAngles = new Vector3(0, 0, 0);
+                block.transform.localScale = new Vector3(2.8f, 1f, 2.8f);
+                block.transform.position = new Vector3(tile.gameObject.transform.position.x, 0, tile.gameObject.transform.position.y * 2);
+                block.AddComponent<NavMeshObstacle>().carving = true;
+                block.GetComponent<Renderer>().enabled = false;
+                blocks.Add(block);
+            }
+        }
+
+        private void UnBlock()
+        {
+            if (blocks != null)
+            {
+                blocks.ForEach(block => DestroyImmediate(block));
+                blocks = null;
+            }
+        }
+    }
+}
