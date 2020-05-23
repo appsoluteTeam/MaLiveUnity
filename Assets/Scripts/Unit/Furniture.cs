4dd59c9c76035872c6866e8c5881f15254c18379
wilybear <samerj9712@gmail.com>
2020-05-23 16:58:31 +0900

    ff
    fff

--- Assets/Scripts/Unit/Furniture.cs
+++ Assets/Scripts/Unit/Furniture.cs
@@ -18,7 +18,7 @@ namespace Model
     {
 
         //각 방향에 대한 게임 오브젝트들
-        public GameObject[] ListDirectionItem = new GameObject[4];
+        //public GameObject[] ListDirectionItem = new GameObject[4];
 
         //south가 기본값
         private Direction direction = Direction.South;
@@ -36,12 +36,14 @@ namespace Model
         {
             direction = (Direction)(((int)direction + 1) % 4);
 
-            foreach (var dir in ListDirectionItem)
-                dir.SetActive(false);
+
+            //foreach (var dir in ListDirectionItem)
+            //    dir.SetActive(false);
 
             //gameobject들중 해당 direction만 남기고 나머지 setActive false
-            ListDirectionItem[(int)direction].SetActive(true);
+            //ListDirectionItem[(int)direction].SetActive(true);
             //가로 세로 조정
+            transform.Rotate(0, 90, 0);
             var temp = width;
             width = length;
             length = temp;
@@ -59,11 +61,11 @@ namespace Model
                 length = temp;
             }
             direction = dir;
+            transform.Rotate(0, 90, 0);
+//            foreach (var diritem in ListDirectionItem)
+//                diritem.SetActive(false);
 
-            foreach (var diritem in ListDirectionItem)
-                diritem.SetActive(false);
-
-            ListDirectionItem[(int)dir].SetActive(true);
+        //   ListDirectionItem[(int)dir].SetActive(true);
         }
 
         //게임오브젝트의 위치를 타일의 위치로 옮긴다.
@@ -76,8 +78,7 @@ namespace Model
         //각 방향별 게임오브젝트마다 색깔을 바꾸어줌
         public void SetColor(Color color)
         {
-            foreach (var dir in ListDirectionItem)
-                dir.GetComponent<SpriteRenderer>().color = color;
+            gameObject.GetComponent<SpriteRenderer>().color = color;
         }
 
 
@@ -89,8 +90,7 @@ namespace Model
 
             //랜더링 시키기?
             //layer 호가정
-            foreach (var dir in ListDirectionItem)
-                dir.GetComponent<Renderer>().sortingLayerName = Unit_LAYER;
+            gameObject.GetComponent<Renderer>().sortingLayerName = Unit_LAYER;
 
             //취소했을때를 위하여
             previous = new HistoricalData(origin, direction);
@@ -104,8 +104,7 @@ namespace Model
             tiles = new List<Tile>();
 
             //PREVIEW로 옮김
-            foreach (var dir in ListDirectionItem)
-                dir.GetComponent<Renderer>().sortingLayerName = PREVIEW_LAYER;
+            gameObject.GetComponent<Renderer>().sortingLayerName = PREVIEW_LAYER;
 
             UnBlock();
         }
@@ -119,8 +118,11 @@ namespace Model
                 var block = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 block.transform.SetParent(GameObject.Find("AIManager").transform);
                 block.transform.localEulerAngles = new Vector3(0, 0, 0);
-                block.transform.localScale = new Vector3(2.8f, 1f, 2.8f);
-                block.transform.position = new Vector3(tile.gameObject.transform.position.x, 0, tile.gameObject.transform.position.y * 2);
+                //    block.transform.localScale = new Vector3(2.8f, 1f, 2.8f);
+                block.transform.localScale = new Vector3(1f, 1f, 1f);
+//                block.transform.position = new Vector3(tile.gameObject.transform.position.x, 0, tile.gameObject.transform.position.y * 2);
+
+                block.transform.position = new Vector3(tile.gameObject.transform.position.x, 0, tile.gameObject.transform.position.y);
                 block.AddComponent<NavMeshObstacle>().carving = true;
                 block.GetComponent<Renderer>().enabled = false;
                 blocks.Add(block);
--- Assets/Scripts/Unit/Tile.cs
+++ Assets/Scripts/Unit/Tile.cs
@@ -6,13 +6,16 @@ namespace Model
         public int x { get; private set; }
         public int y { get; private set; }
 
+        public int z { get; private set; }
+
         [SerializeField]
         public bool isBlock = false;
 
-        public void Set(int x, int y)
+        public void Set(int x, int y,int z)
         {
             this.x = x;
             this.y = y;
+            this.z = z;
         }
     }
 }
